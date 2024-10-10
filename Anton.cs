4a1daf65ff5d34764c2c2b9f0f49b6cffba271c7
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-10-10 15:00:39 +0200

    antons commit

---
+++ Anton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConflictsOnGitHub
+{
+    internal class Anton
+    {
+        public void Hello()
+        {
+            Console.WriteLine("Anton says hello");
+        }
+    }
+}
