b3e6628c6fc871ec36ae8219b0bcf9c4c42ee3e5
onni82 <onni.bucht@me.com>
2024-10-10 15:06:39 +0200

    created a branch and made a new class named Onni

---
+++ onni.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConflictsOnGitHub
+{
+	public class Onni
+	{
+		static void Onni()
+        {
+            Console.WriteLine("Onni was here.");
+        }
+    }
+}
