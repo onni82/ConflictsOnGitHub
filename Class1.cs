bb72a62ab8ed47ae1b1c7eedeac20d8c2e5dac3d
Michael Wortel <michael.wortel@chasacademy.se>
2024-10-10 15:09:16 +0200

    best change

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConflictsOnGitHub
+{
+    internal class Class1
+    {
+        string name = "senpai";
+        
+        public void BestMethod()
+        {
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine($"{name}? kekw");
+        }
+    }
+}
